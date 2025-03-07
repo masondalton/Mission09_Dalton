128cb7745176d2596f4f0b23b2a5573115819a89
masondalton <mcoled@byu.edu>
2025-03-07 14:48:55 -0700

    Test, added functionality for page outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 
+// Heading section to introduce user to the site
 function Welcome() {
   <h1>March Madness Teams</h1>;
 }
+
+// Team Card
+
+// List of team cards
+
 function App() {
   return <>Welcome()</>;
 }
