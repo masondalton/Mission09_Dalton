139a117759e628935b351ad7849de67d27e6d664
masondalton <mcoled@byu.edu>
2025-03-07 16:12:49 -0700

    Finished with the logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,79 @@
 import "./App.css";
+import teamsData from "./CollegeBasketballTeams.json";
+console.log(teamsData);
+const teams = teamsData.teams;
+console.log(teamsData);
 
 // Heading section to introduce user to the site
 function Welcome() {
-  <h1>March Madness Teams</h1>;
+  return <h1>March Madness Teams</h1>;
 }
 
 // Team Card
-
+function Team({
+  tid,
+  cid,
+  did,
+  school,
+  name,
+  abbrev,
+  pop,
+  city,
+  state,
+  latitude,
+  longitude,
+}: {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}) {
+  return (
+    <>
+      <div>
+        <h2>
+          The {name} ({tid})
+        </h2>
+        <h3>
+          of {school} ({abbrev})
+        </h3>
+        <h4>
+          From {city}, {state} located at (lat: {latitude}, long: {longitude})
+        </h4>
+        <h4>has a population of {pop}</h4>
+        <h5>
+          Conference: {cid} Division: {did}
+        </h5>
+        <br></br>
+      </div>
+    </>
+  );
+}
 // List of team cards
+function TeamList() {
+  return (
+    <>
+      {teams.map((singleTeam) => (
+        <Team key={singleTeam.tid} {...singleTeam} />
+      ))}
+    </>
+  );
+}
 
 function App() {
-  return <>Welcome()</>;
+  return (
+    <>
+      <Welcome />
+      <TeamList />
+    </>
+  );
 }
 
 export default App;
